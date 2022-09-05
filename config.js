4f43bf0334e987ae7d08663d4663853f60832e98
雨 ʀᴀʏʜᴀɴ サンダー <102699105+RyhnXD@users.noreply.github.com>
2022-09-05 20:08:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ global.sc = 'https://youtu.be/67755' //jngn ganti hargai creator
 global.ss = 'ᴀᴅᴀ' //jngn ganti hargai creator
 global.sss = 'ᴀᴅᴀ' //jngn ganti hargai creator
 global.yt = 'ɢᴀ ᴀᴅᴀ'
-global.dana = '085828764046' //ganti pakai nomor dana mu
-global.pulsa = '085828764046' //ganti pakai nomor pulsa mu
-global.gopay = '085828764046' //ganti pakai nomor Gopay mu
+global.dana = 'ga ada :v' //ganti pakai nomor dana mu
+global.pulsa = '0895627514070' //ganti pakai nomor pulsa mu
+global.gopay = 'Ga ada :v' //ganti pakai nomor Gopay mu
 
 //=============『 Info Bot 』=================//
 global.namebot = 'ʟᴜᴍɪɴᴏᴜs-ᴍᴅ' //ganti jadiin nama bot mu
@@ -33,15 +33,15 @@ global.nomer = 'https://wa.me/62895627514070' //ganti jadi nomor mu
 global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》PERMANEN Rp 10.000\nMAKSIMAL 2 GRUP\n》PERMANEN  Rp 20.000\nMAKSIMAL 6 GRUP \n》1 BULAN Rp 5.000\nMAKSIMAL 1 GRUP'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/43b9d77d06a0493708262.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/43b9d77d06a0493708262.jpg' //ini juga ganti pakai foto bot mu
-global.fotonya3 = 'https://telegra.ph/file/183c6b67b4fa8bd438332.jpg' //ini juga ganti
-global.fotonya4 = 'https://telegra.ph/file/520aa035804ae644ba8db.jpg' //ini juga ganti
-global.fotonya5 = 'https://telegra.ph/file/183c6b67b4fa8bd438332.jpg' //ini juga ganti
-global.fotonya6 = 'https://telegra.ph/file/43b9d77d06a0493708262.jpg' //ini ganti pakai foto sosialmedia mu
-global.fotonya7 = 'https://telegra.ph/file/43b9d77d06a0493708262.jpg' //ini juga ganti
-global.fotonya8 = 'https://telegra.ph/file/14ec0e0c93af4812d1fd7.jpg' //foto session
-global.donasi = 'https://telegra.ph/file/43b9d77d06a0493708262.jpg' //ganti pakai poto payment atau apa
+global.fotonya1 = 'https://telegra.ph/file/4ebea141a2d110c9959f6.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/4ebea141a2d110c9959f6.jpg' //ini juga ganti pakai foto bot mu
+global.fotonya3 = 'https://telegra.ph/file/096f342e046a046e0cae7.jpg' //ini juga ganti
+global.fotonya4 = 'https://telegra.ph/file/3b4d1b83ac03f9590e1ac.jpg' //ini juga ganti
+global.fotonya5 = 'https://telegra.ph/file/4ebea141a2d110c9959f6.jpg' //ini juga ganti
+global.fotonya6 = 'https://telegra.ph/file/99d059d8b69a6740af1c6.jpg' //ini ganti pakai foto sosialmedia mu
+global.fotonya7 = 'https://telegra.ph/file/99d059d8b69a6740af1c6.jpg' //ini juga ganti
+global.fotonya8 = 'https://telegra.ph/file/4ebea141a2d110c9959f6.jpg' //foto session
+global.donasi = 'https://telegra.ph/file/fe1253635bf8472b66bce.jpg' //ganti pakai poto payment atau apa
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'ʟᴜᴍɪɴᴏᴜs-ᴍᴅ'
@@ -50,7 +50,7 @@ global.wm2 = 'ᯤ ʟᴜᴍɪɴᴏᴜs-ᴍᴅ'
 global.wm3 = 'ᯤ ʀᴀʏ 𝙾𝙵𝙲'
 global.htki = '––––––『' 
 global.htka = '』––––––'
-global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
+global.media = 'https://telegra.ph/file/d6fe8c2802bf1a5e82692.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wait = '*sᴇᴅᴀɴɢ ᴅɪᴘʀᴏsᴇs*'
 global.eror = '_*ᴇʀʀᴏʀ*_'
