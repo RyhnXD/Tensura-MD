924fcc1cd76c151075832d13db3f477418ca57ce
雨 ʀᴀʏʜᴀɴ サンダー <102699105+RyhnXD@users.noreply.github.com>
2022-09-05 20:25:51 +0700

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/2b9a839b602942c447414.png', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/2b9a839b602942c447414.png', m, { packname: "ʟᴜᴍɪɴᴏᴜs-ᴍᴅ ʙʏ", author: "ʜᴀɴ" })
 }
 
 handler.customPrefix = /^(ok)$/i
